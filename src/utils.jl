545aad0f5560035ec8589cf190f377c957068b0a
Théo Galy-Fajou <theo.galyfajou@gmail.com>
2020-09-28 18:49:18 +0200

    Added implementation for inverse BlockDiagonals.

--- src/utils.jl
+++ src/utils.jl
@@ -8,16 +8,16 @@ end
 
 function update(td::TuringDenseMvNormal{<:AbstractVector, <:Cholesky{<:Real, <:BlockDiagonal}}, θ::AbstractArray)
     μ = θ[1:length(td)]
-    sizes = vcat(0, blocksizes(td.C.U))
-    ids = cumsum(sizes .* (sizes .+ 1) / 2)
+    sizes = vcat(0, first.(blocksizes(td.C.U)))
+    ids = cumsum((sizes .* (sizes .+ 1)) ÷ 2)
     Σ = θ[(length(td)+1):end]
-    Σs = [make_triangular(view(Σ, (ids[i]+1):ids[i+1]), sizes[i+1]) for i in 1:(length(ids)-1)]
+    Σs = [make_triangular(Σ[(ids[i-1]+1):ids[i]], sizes[i]) for i in 2:length(ids)]
     return update(td, μ, BlockDiagonal(Σs))
 end
 
 update(d::TuringDenseMvNormal, μ, L) = TuringDenseMvNormal(μ, L * L' + 1e-5I)
 
-function update(td::Union{TransformedDistribution{<:TuringDenseMvNormal},TuringDenseMvNormal}, θ::AbstractArray)
+function update(td::TuringDenseMvNormal, θ::AbstractArray)
     μ, L = θ[1:length(td)], make_triangular(θ[length(td) + 1:end], length(td))
     return update(td, μ, L)
 end
@@ -37,4 +37,15 @@ end
 
 function hp_grad(vo, alg, q, logπ, hyperparameters, args...)
     ForwardDiff.gradient(x -> vo(alg, q, logπ(x), args...), hyperparameters)
+end
+
+function LinearAlgebra.:\(A::BlockDiagonal, B::AbstractVecOrMat)
+    i = 1
+    c = similar(B)
+    for a in blocks(A)
+        d = size(a, 1)
+        c[i:i+d-1, :] = a \ B[i:i+d-1, :]
+        i += d
+    end
+    return c
 end
